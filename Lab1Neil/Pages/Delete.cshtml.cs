02ab5e101389960bf2a90d9696b8217d08bedb0b
PhArObiS <neil.lopes.79@gmail.com>
2023-09-28 11:46:38 -0400

    Chage Project Name

--- Lab1Neil/Pages/Delete.cshtml.cs
+++ Lab1Neil/Pages/Delete.cshtml.cs
@@ -1,3 +1,5 @@
+using Lab1Neil.DataSource;
+using Lab1Neil.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,14 @@ namespace Lab1Neil.Pages
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+
+        public StudentData dataSource = StudentData.GetInstance();
+
+        public Student Student { get; set; }
+
+        public void OnGet(string id)
         {
+            Student = dataSource.myList.FirstOrDefault(x => x.StudentNumber == id);
         }
     }
 }
