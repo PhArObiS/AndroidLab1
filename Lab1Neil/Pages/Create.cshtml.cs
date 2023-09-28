60fa63e1fde0268c397c11600f45ee08186b8909
PhArObiS <neil.lopes.79@gmail.com>
2023-09-28 11:35:37 -0400

    Add project files.

---
+++ Lab1Neil/Pages/Create.cshtml.cs
@@ -0,0 +1,22 @@
+using Lab1Neil.DataSource;
+using Lab1Neil.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab1Neil.Pages
+{
+    public class CreateModel : PageModel
+    {
+
+        public StudentData dataSource = StudentData.GetInstance();
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost(string StudentNumber, string Email, string Password, string Image, string FullName )
+        {
+            dataSource.myList.Add(new Student(StudentNumber, FullName, Email, Password, Image));
+        }
+    }
+}
---
+++ Lab1Neil/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Lab1Neil.DataSource;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab1Neil.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public StudentData dataSource = StudentData.GetInstance();
+
+        public IndexModel()
+        {
+            
+        }
+
+        public void OnGet()
+        {
+
+
+        }
+    }
+}
